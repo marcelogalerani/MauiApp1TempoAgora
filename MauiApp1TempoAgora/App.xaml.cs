df6203940e117949e7114a3a1ff14d17424807bd
marcelogalerani <marcelo.galerani@etec.sp.gov.br>
2025-04-02 14:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1TempoAgora/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1TempoAgora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApp1TempoAgora/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1TempoAgora
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
